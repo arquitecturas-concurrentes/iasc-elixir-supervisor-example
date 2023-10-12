4fb85c21114a713ac2af73ee86de9f23f0b7d88c
Ernesto Bossi <bossi.ernestog@gmail.com>
2023-10-12 18:33:43 -0300

    2023c2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule SupEjemplo.MixProject do
     [
       app: :sup_ejemplo,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
